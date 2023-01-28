b205891245f6c18cc98685fa78734b3e59bd0486
alifakbar111 <alifakbar111@gmail.com>
2023-01-28 14:05:25 +0700

    feat: player layout component

---
+++ components/playerLayout.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      Layout
+      {children}
+    </Box>
+  )
+}
+
+export default PlayerLayout
