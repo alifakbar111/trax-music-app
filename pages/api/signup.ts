a6176201aa5c3e2b83cebfa4bdce30863ecc3138
alifakbar111 <alifakbar111@gmail.com>
2023-02-03 15:33:05 +0700

    feat: create signup api

---
+++ pages/api/signup.ts
@@ -0,0 +1,49 @@
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie'
+import bcrypt from 'bcrypt'
+import { NextApiResponse, NextApiRequest } from 'next'
+import prisma from '../../lib/prisma'
+import { User } from '../../types/user'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync()
+  const { email, password } = req.body
+
+  let user: User
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    })
+  } catch (e) {
+    res.status(401)
+    res.json({ error: 'User already exists' })
+    return
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    'hello',
+    { expiresIn: '8h' }
+  )
+
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('TRAX_ACCESS_TOKEN', token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: '/',
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+    })
+  )
+
+  res.json(user)
+}
---
+++ types/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  email: string
+  id: number
+  createdAt?: Date
+  updatedAt?: Date
+  password?: string
+}
