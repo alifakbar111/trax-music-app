9e0ff20f8a1f1a3c3801bffae6701e02f647d297
alifakbar111 <alifakbar111@gmail.com>
2023-02-04 16:52:33 +0700

    feat: fetchin playlist data

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -18,6 +18,7 @@ import {
   MdFavorite,
 } from 'react-icons/md'
 import { Menu } from '../types/sidebar'
+import { usePlaylist } from '../lib/hooks'
 
 const navMenu: Menu[] = [
   {
@@ -50,9 +51,11 @@ const musicMenu: Menu[] = [
   },
 ]
 
-const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
+// const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
 
 const Sidebar = () => {
+  const { playlists } = usePlaylist()
+
   return (
     <Box
       width="100%"
@@ -109,10 +112,10 @@ const Sidebar = () => {
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
             {playlists.map((playlist) => (
-              <ListItem paddingX="20px" key={playlist}>
+              <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/">
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
